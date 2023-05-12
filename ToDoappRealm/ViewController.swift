182c2bd64495f0a615ecca118fbbd5580328b1b2
Deepthi <deepthi.s@westagilelabs.com>
2023-05-12 11:21:41 +0530

    Initial Commit

---
+++ ToDoappRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoappRealm
+//
+//  Created by West Agile Labs on 12/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
