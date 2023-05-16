e5c7e753c8005da121875f6a9a70b9c496c29c24
Deepthi <deepthi.s@westagilelabs.com>
2023-05-16 20:50:27 +0530

    changes added

--- ToDoappRealm/ViewController.swift
+++ ToDoappRealm/ViewController.swift
@@ -5,15 +5,123 @@
 //  Created by West Agile Labs on 12/05/23.
 //
 
+import RealmSwift
 import UIKit
+
+class Contact: Object{
+    @Persisted var firstname: String
+    @Persisted var lastname: String
+    
+   convenience init(firstname: String, lastname: String){
+       self.init()
+       self.firstname=firstname
+       self.lastname=lastname
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contactTableView: UITableView!
+    var contactArray = [Contact]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuration()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addContactButtonTapped(_ sender: UIBarButtonItem) {
+        contactConfiguration(isAdd: true)
+        
+    }
+    
 }
+extension ViewController{
+    func configuration(){
+        contactTableView.delegate=self
+        contactTableView.dataSource=self
+        contactTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        contactArray = DatabaseHelper.shared.getAllContacts()
+        
+    }
+    func contactConfiguration(isAdd : Bool, index: Int=0){
+        let alertController = UIAlertController(title: isAdd ? "Add Contact" : "Update Contact", message: isAdd ? "Enter your contact details" : "Update your contact details", preferredStyle: .alert)
+        let save = UIAlertAction(title: isAdd ? "Save" : "update", style: .default) { _ in
+            if let firstname = alertController.textFields?.first?.text,
+               let lastname = alertController.textFields?[1].text{
+                let contact = Contact(firstname: firstname, lastname: lastname)
+                if isAdd{
+                    self.contactArray.append(contact)
+                    DatabaseHelper.shared.saveContact(contact: contact)
+                }
+                
+                else{
+                    //self.contactArray[index] = contact
+                    DatabaseHelper.shared.updateContact(oldContact: self.contactArray[index], newContact: contact)
+                }
+                
+                self.contactTableView.reloadData()
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addTextField { firstnameField in
+       
+            firstnameField.placeholder = isAdd ? "Enter your firstname" :
+            self.contactArray[index].firstname
+            
+       
+            
+        }
+        alertController.addTextField{ lastnameField in
+            
+            lastnameField.placeholder = isAdd ? "Enter your lastname" : self.contactArray[index].lastname
+            
+            
+        }
+        
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        present(alertController, animated: true)
+    }
+    }
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        contactArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        else{
+           return UITableViewCell()
+        }
+        cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = contactArray[indexPath.row].firstname
+        cell.detailTextLabel?.text = contactArray[indexPath.row].lastname
+        return cell
+    }
+}
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let edit = UIContextualAction(style: .normal, title:"Edit") {_, _, _ in
+            self.contactConfiguration(isAdd: false, index: indexPath.row)
+            
+        
+            
+        }
+        edit.backgroundColor = .systemMint
+        let delete = UIContextualAction(style: .destructive, title: "Delete") {
+            _, _, _ in
+            print(self.contactArray[indexPath.row])
+            DatabaseHelper.shared.deleteContact(contact: self.contactArray[indexPath.row])
+            self.contactArray.remove(at: indexPath.row)
+            self.contactTableView.reloadData()
+            
+        }
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [delete, edit])
+        return swipeConfiguration
+    }
+}
+
+
+
+
 
